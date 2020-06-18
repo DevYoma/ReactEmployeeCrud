097740611534d9eb377e7b18abeebb4360ff08f8
DevYoma <Lawrenceyoma@gmail.com>
2020-06-18 22:19:05 +0100

    changed the name again

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn yoma
+          Emore Ogheneyoma Lawrence
         </a>
       </header>
     </div>
